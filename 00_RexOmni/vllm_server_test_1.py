d7e125bd513701c65b6e0791a2738419aa57a4ef
xlshaoscu <xlshaoscu@foxmail.com>
2026-03-30 02:58:05 +0000

    add error

---
+++ 00_RexOmni/vllm_server_test_1.py
@@ -0,0 +1,21 @@
+import base64
+import requests
+
+path = "/home/s00964975/00_Software/Rex-Omni/tutorials/detection_example/test_images/boys.jpg"
+with open(path, "rb") as f:
+    img = base64.b64encode(f.read()).decode()
+
+res = requests.post("http://127.0.0.1:8000/v1/chat/completions", json={
+    "model": "/opt/data/models/IDEA-Research/Rex-Omni",
+    "messages": [{
+        "role": "user",
+        "content": [
+            {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{img}"}},
+            {"type": "text", "text": "Detect all persons. Output only [x0,y0,x1,y1] per line."}
+        ]
+    }],
+    "temperature": 0,
+    "max_tokens": 40000
+})
+
+print(res.json())
